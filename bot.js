96da87d10e3b33c3570f1a1ddd89daf1e3775579
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-16 00:20:53 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('MjU3MTExNDc2NDA0MjI0MDAx.DYmG3w.UQyBPtQ3nHcrzFU0_jG4aiqIgxY');
