381bd9786c8d164368f4f4105b4caf552079a580
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-17 20:08:02 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,47 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', message => {
+var prefix = "@";
+
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id == 410835593451405312) return;
+
+
+if (message.content.startsWith(prefix + 'playing')) {
+  if (message.author.id !== 'botownerid') return message.reply('**only for the botowner **')
+client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult}** : Playing changed to`)
+    message.react("👌")
+
+} else
+
+if (message.content.startsWith(prefix + 'setname')) {
+  if (message.author.id !== 'botownerid') return message.reply('**only for the botowner **')
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`**${argresult}** : done`)
+} else
+
+if (message.content.startsWith(prefix + 'streem')) {
+  if (message.author.id !== 'botownerid') return message.reply('**only for the botowner **')
+client.user.setGame(argresult, "http://twitch.tv/y04zgamer");
+    message.channel.sendMessage(`**${argresult}** streem changed to`)
+    message.react("👌")
+
+} else
+
+
+
+if (message.content.startsWith(prefix + 'watching')) {
+  if (message.author.id !== 'botownerid') return message.reply('**only for the botowner **')
+    client.user.setActivity(argresult, {type : 'watching'});
+ message.channel.sendMessage(`**${argresult}** : Watching changed to`)
+ message.react("👌")
+
+}
+
+ });
+
 client.login('MjU3MTExNDc2NDA0MjI0MDAx.DYmG3w.UQyBPtQ3nHcrzFU0_jG4aiqIgxY');
