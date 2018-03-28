135843a68c61861041c84b2bdc9d0f63067996de
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-28 20:15:09 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,13 +47,234 @@ client.on('message', msg => {
 
 
 
+client.on('message', message => {
+    if (message.content === "-FORT NIGHT") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "FORT NIGHT"));
+  }
+    
+});
 
 
 
 
+client.on('message', msg => {
+  if (msg.content === '-FORT NIGHT') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
 
 
 
+
+client.on('message', message => {
+    if (message.content === "-RUIES OF SURVIVAL") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "RUIES OF SURVIVAL"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-RUIES OF SURVIVAL') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+
+
+client.on('message', message => {
+    if (message.content === "-CALL OF DUTY") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "CALL OF DUTY"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-CALL OF DUTY') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+
+
+client.on('message', message => {
+    if (message.content === "-TOOMB RIDER") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "TOOMB RIDER"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-TOOMB RIDER') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+
+
+
+client.on('message', message => {
+    if (message.content === "-PUPG") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "PUPG"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-PUPG') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+
+
+
+client.on('message', message => {
+    if (message.content === "-GRAN TRESMO") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "GRAN TRESMO"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-GRAN TRESMO') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+
+
+
+client.on('message', message => {
+    if (message.content === "-BRAWAHALA") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "BRAWAHALA"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-BRAWAHALA') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+
+
+
+client.on('message', message => {
+    if (message.content === "-HANK1") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "HANK1"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-HANK1') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+--**-*-*-*
+
+client.on('message', message => {
+    if (message.content === "-HANK2") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "HANK2"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-HANK2') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+client.on('message', message => {
+    if (message.content === "-CAUNTER STRIKE") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "CAUNTER STRIKE"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-CAUNTER STRIKE') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+
+
+
+client.on('message', message => {
+    if (message.content === "-ROKET LGE") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "ROKET LGE"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-ROKET LGE') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
+
+
+client.on('message', message => {
+    if (message.content === "-LEUGE") {
+        if(!message.channel.guild) return;
+        message.member.addRole(message.guild.roles.find("name", "LEUGE"));
+  }
+    
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === '-LEUGE') {
+    msg.reply('تُمَّ الْاِخْتِيَارُ العبه :white_check_mark: ');
+  }
+});
 
 
 
